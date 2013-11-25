8982c8db67bc6f694cca6e200a534b454ed4ea90
Nabanita Sarker <nabanita09.sarker@gmail.com>
2013-11-25 20:32:31 +0530

    reflection

---
+++ reflection.js
@@ -0,0 +1,8 @@
+#!/usr/local/bin/node
+
+var person = {"name":"John","age":"30","location":"UK"};
+
+console.log(typeof(person));
+console.log(typeof(person.name));
+console.log(person.toString());
+console.log(typeof(person.toString));
