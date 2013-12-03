8d818b1b167662ba3f18c66900800d0879280e7f
Nabanita Sarker <nabanitas@rspc1007.(none)>
2013-12-03 15:55:24 +0530

    unit testCases

---
+++ src/array-functions.js
@@ -0,0 +1,162 @@
+#!/usr/bin/node
+
+exports.count = function(arr,p){
+	var counter = 0;
+	var i;
+	for(i = 0; i < arr.length; i++){
+		if(p(arr[i])){
+			counter++;
+		}
+	}
+	return counter;
+}
+
+
+/* head function */
+exports.head = function(a){
+	return a[0];
+}
+
+/* init function */
+exports.init = function(arr){
+	return arr.slice(0,arr.length-1);
+}
+
+
+/* last function */
+exports.last = function(arr){
+	return arr.slice(arr.length-1);
+}
+
+
+/* tail function */
+exports.tail = function(arr){
+	return arr.slice(1);
+}
+
+/* distinct function */
+/* var distinct = function(arr){
+	var a = [];
+	var i;
+	for(i = 0; i < arr.length; i++){
+		if(a.indexOf(arr[i])==-1){
+			a.push(arr[i]);
+		}
+	}
+	return a;
+}
+var array = [1,2,1,2,3];
+console.log(distinct(array)); */
+
+
+/* dropRight function */
+/* var dropRight = function(arr,n){
+	return arr.slice(0,arr.length-n);
+}
+var array = [1,2,3,4,5,6,7];
+console.log(dropRight(array,3)); */
+
+
+/* dropWhile function */
+/* var dropWhile = function(arr,p){
+	var i;
+	var a = [];
+	for(i = 0; i < arr.length; i++){
+		if(!p(arr[i])){
+			a.push(arr[i]);
+		}
+	}
+	
+	return a;
+}
+var isOdd = function(n){
+	return (n%2)!=0;
+}
+var array = [1,2,3,4,5];
+console.log(dropWhile(array,isOdd)); */
+
+
+/* endsWith function */
+/* var endsWith = function(a,b){
+ 	var i = a.length-1;
+ 	var j = b.length-1;
+ 	for(j = b.length-1; j >= 0; j--){
+ 		if(b[j] != a[i]){
+ 			return false;
+ 		}
+ 		i--;
+ 	}
+ 	return true;
+ }
+
+ console.log(endsWith([1,2,3,4,5],[7,8,9])); */
+
+
+ /* filter function */
+/* var filter = function(arr,p){
+ 		var i;
+ 		var a = [];
+ 		for(i = 0; i <= arr.length; i++){
+ 			if(p(arr[i])){
+ 				a.push(arr[i]);
+ 			}
+ 		}
+ 		return a;
+ }
+
+ var isEven = function(n){
+ 	return (n%2) == 0;
+ }
+
+ var array = [1,2,3,4,5,6,7,8,9,10];
+ console.log(filter(array,isEven)); */
+
+
+ /* filterNot function */
+/*  var filterNot = function(arr,p){
+ 	var a = [];
+ 	var i;
+ 	for(i = 0; i < arr.length; i++){
+ 		if(!p(arr[i])){
+ 			a.push(arr[i]);
+ 		}
+ 	}
+ 	return a;
+ }
+
+var isEven = function(n){
+	return (n%2) == 0
+}
+
+ var array = [1,2,3,4,5,6,7,8,9,10];
+ console.log(filterNot(array,isEven)); */
+
+
+ /* find function */
+/* var find = function(arr,p){
+ 	var a = [];
+ 	var i;
+ 	for(i = 0; i <= arr.length; i++){
+ 		if(p(arr[i])){
+ 			a.push(arr[i]);
+ 			return a;
+ 		}
+ 	}
+ 	return;
+ }
+
+ var isEven = function(n){
+ 	return (n%2) == 0;
+ }
+
+ var array = [1,2,3,4,5,6,7,8,9,10];
+ console.log(find(array,isEven)); */
+
+
+ /* flatten function */
+/*  var flatten = function(a,b){
+ 	var c = [];
+ 	c = a.concat(b);
+ 	return c;
+ }
+ console.log(flatten([1,2,3],[1,2,3])); */
---
+++ tests/tests-array-functions.js
@@ -0,0 +1,88 @@
+var nodeunit = require('nodeunit');
+var arrays = require('../src/array-functions');
+
+exports['count'] = nodeunit.testCase({
+
+'an empty array': function (test) {
+		var isEven = function(n) {
+			return (n%2) == 0;
+		};
+
+	 test.equal(arrays.count([], isEven), 0);
+	 test.done();
+},
+
+'numeric arrays with predicate always returning true': function (test) {
+		var alwaysTrue = function(n) {
+			return true;
+		};
+
+	 test.equal(arrays.count([2, 4, 6, 8], alwaysTrue), 4);
+	 test.done();
+},
+
+'numeric arrays with predicate always returning false': function (test) {
+		var alwaysFalse = function(n) {
+			return false;
+		};
+
+	 test.equal(arrays.count([2, 4, 6, 8], alwaysFalse), 0);
+	 test.done();
+},
+
+'string arrays' : function(test) {
+	var actual = arrays.count(["one", "two", "three"], function(e){
+		return e.length <= 3;
+	});
+
+	test.equal(actual, 2)
+	test.done();
+}
+});
+
+/* Test Cases for Head Function */
+
+exports['head'] = nodeunit.testCase({
+'Head function should return first element of an array' : function(test) {
+	test.equal(arrays.head([1,2,3,4]),1);
+	test.done();
+},
+'Head function with empty array should return undefined' : function(test) {
+	test.equal(typeof (arrays.head([])),'undefined');
+	test.done();
+},
+'String array should return first string' : function(test) {
+	test.equal(arrays.head(["one","two","three"]),"one");
+	test.done();
+}
+});
+
+/* Test Cases of init function */
+
+exports['init'] = nodeunit.testCase({
+/*'Init function should return all elements except the last one from an array' : function(test) {
+a	test.equal(arrays.init([1,2,3,4,5]), [1,2,3,4]);
+	test.done();
+},*/
+
+'Init function with empty array should return undefined' : function(test) {
+	test.equal(typeof (arrays.init([])),'object');
+	test.done();
+}
+});
+
+/* Test Cases of Last function*/
+
+exports['last'] = nodeunit.testCase({
+'Last function should return last element of an array' : function(test) {
+	test.equal(arrays.last([1,2,3,4,5]),5);
+	test.done();
+},
+
+'last function with an empty array' : function(test) {
+	test.equal(typeof (arrays.last([])),'object');
+	test.done();
+}
+});
+
+
