f444c688856089c6c815eeb6226880a68d3ab9cb
Nabanita Sarker <nabanitas@rspc1007.(none)>
2013-12-11 15:08:37 +0530

    keys

--- src/object-functions.js
+++ src/object-functions.js
@@ -9,5 +9,29 @@ exports.Values = function(obj){
         return a;
 }
 
+var Keys = function(obj){
+        var i;
+        var a = [];
+        for (i in obj){
+                console.log(i);
+                a.push[(i)];
+        }
+        return a;
+}
+var object = new Keys({one:1,two:2,three:3});
+console.log(Keys(object));
+
+var Pairs = function(obj){
+        var i;
+        var a = [];
+        for (i in obj){
+                //console.log(i + "," + obj[i]);
+                a.push([[i] + "," + obj[i]]);
+        }
+        return a;
+}
+var object = new Pairs({one:1,two:2,three:3});
+console.log(Pairs(object));
+
 
 
